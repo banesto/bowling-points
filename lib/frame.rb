313953a2af2197939d600c07f48aae835a565645
Ernests Kečko <ernests.kecko@gmail.com>
2022-05-22 18:26:33 +0300

    Score calculation for spares & strikes

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,8 +1,10 @@
 class Frame
-  attr_accessor :balls, :score, :is_open_score
+  @@score = 0
+  attr_accessor :balls, :score, :is_open_score, :bonus_pins
 
   def initialize
     self.balls = []
+    self.bonus_pins = []
     self.is_open_score = false
     self.score = 0
   end
@@ -15,6 +17,17 @@ class Frame
     self.balls << pins
     self.score += pins
     self.is_open_score = true if balls.sum == 10
+    true
+  end
+
+  def add_bonus_pins(pins)
+    return unless is_open_score
+
+    self.bonus_pins << pins
+
+    if (bonus_pins.size == 1 && spare?) || (bonus_pins.size == 2 && strike?)
+      calculate_score
+    end
   end
 
   def strike?
@@ -27,5 +40,26 @@ class Frame
 
   def finished?
     balls.sum == 10 || balls.size == 2
+  end
+
+  def to_s
+    "balls: #{balls} | score: #{score unless is_open_score}"
+  end
+
+  def simple_score
+    return unless balls.sum < 10
+
+    self.score += @@score
+    @@score = score
+  end
+
+  def calculate_score
+    self.score = @@score + balls.sum + bonus_pins.sum
+    @@score = score
+    self.is_open_score = false
+  end
+
+  def self.reset_score
+    @@score = 0
   end
 end
